267ecdc94d259905ed1800be09185c8dcb1fa7cf
reome <shub29@naver.com>
2023-06-14 19:50:34 +0900

    선택정렬, 삽입정렬
    선택정렬, 삽입정렬 추가

--- CSharpBasic/SortAlgorithm/Program.cs
+++ CSharpBasic/SortAlgorithm/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SortAlgorithm
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace SortAlgorithm
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//        }
+//    }
+//}
+using System.Linq;
+using SortAlgorithm;
+using System.Diagnostics;
+
+int[] arr = { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0 };
+Random random = new Random();
+arr = Enumerable.Repeat(0,100000).Select(i => random.Next(0,100000)).ToArray();
+
+Stopwatch stopWatch = Stopwatch.StartNew();
+
+SortAlgorithm.SortAlgorithms.BubbleSort(arr); //10만개 31818ms
+//SortAlgorithm.SortAlgorithms.SelectionSort(arr); //10만개 7467ms
+//SortAlgorithm.SortAlgorithms.InsertionSort(arr);  //10만개 4750ms
+
+stopWatch.Stop();
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.Write($"{arr[i]}, ");
+//}
+Console.WriteLine();
+Console.WriteLine(stopWatch.ElapsedMilliseconds);
--- CSharpBasic/SortAlgorithm/SortAlgorithms.cs
+++ CSharpBasic/SortAlgorithm/SortAlgorithms.cs
@@ -32,6 +32,60 @@ namespace SortAlgorithm
         }
         #endregion
 
+        #region Selection Sort
+        //선택정렬
+        //처음부터 끝까지 순회하면서 현재 값보다 작은 값을 가진 인덱스를 찾아 스왑
+        //한번 Outer 돌 때마다 가장 작은 인덱스가 고정됨
+        //O(N^2)
+        //밖 - n번 돌음, 안 - n/2번 돌음
+        //기본적으로는 unstable
+        public static void SelectionSort(int[] arr)
+        {
+            int minIdx = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                minIdx = i;
+                // i 뒤의 가장 작은 값을 가진 인덱스 찾기
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                }
+
+                // i 뒤에 더 작은값의 인덱스를 찾았으면 스왑
+                if (i != minIdx)
+                    Swap(ref arr[i], ref arr[minIdx]);
+            }
+        }
+        #endregion
+
+        #region Insertion Sort
+        //삽입정렬
+        //현재 위치보다 이전위치들중에서 더 큰값이 있으면 더 큰 값으로 현재 위치에 덮어쓰고
+        //덮어쓰기가 끝나면 마지막으로 덮어쓸 때 참조했던 위치에 현재 위치 값을 덮어씀
+        //현재 탐색 위치 이전까지의 모든 인덱스가 정렬된 상태를 유지하면서 정렬 진행
+        //Stable
+        //O(N^2)
+        public static void InsertionSort(int[] arr)
+        {
+            int j, key; //현재 탐색 중인 숫자 넣을 변수 key
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                key = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        #endregion
+
         //그냥 int a, int b로 하면 값복사돼서 배열에선 안바뀜
         //배열의 값을 바꾸려면 배열의 요소의 주소를 참조해야함
 
